8947ff47b3bab1c8a9918027a7f01c66bdb68497
Allan Hipolito <allanhipolito66@gmail.com>
2020-05-05 07:47:13 -0300

    Projeto Criado com sucesso

---
+++ src/main/java/com/cloudbook/projeto/CloubookApplication.java
@@ -0,0 +1,13 @@
+package com.cloudbook.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloubookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloubookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudbook/projeto/CloubookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudbook.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloubookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
